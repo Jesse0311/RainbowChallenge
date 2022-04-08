bad219ccf77e7ab0f0f279bfb8bcc875fcb300ac
Jesse0311 <j.christians@lab.clicklearning.org>
2022-04-08 15:38:46 +0200

    rainbow challenge button

---
+++ Rainbow.js
@@ -0,0 +1,30 @@
+var myButton = document.getElementById("red")
+var myButton2 = document.getElementById("orange")
+var myButton3 = document.getElementById("yellow")
+var myButton4 = document.getElementById("green")
+var myButton5 = document.getElementById("blue")
+var myButton6 = document.getElementById("purple")
+var myButton7 = document.getElementById("violet")
+var count = 0
+
+myButton.addEventListener('click', function(){
+    document.getElementById("red").style.backgroundColor = 'red';
+})
+myButton2.addEventListener('click', function(){
+    document.getElementById("orange").style.backgroundColor = 'orange';
+})
+myButton3.addEventListener('click', function(){
+    document.getElementById("yellow").style.backgroundColor = 'yellow';
+})
+myButton4.addEventListener('click', function(){
+    document.getElementById("green").style.backgroundColor = 'green';
+})
+myButton5.addEventListener('click', function(){
+    document.getElementById("blue").style.backgroundColor = 'blue';
+})
+myButton6.addEventListener('click', function(){
+    document.getElementById("purple").style.backgroundColor = 'purple';
+})
+myButton7.addEventListener('click', function(){
+    document.getElementById("violet").style.backgroundColor = 'violet';
+})
